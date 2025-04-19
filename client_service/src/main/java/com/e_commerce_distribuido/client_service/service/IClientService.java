9fcfe32d3cc62f6d035e9186603637497deb32e2
Felipe villa <andresfelipevillapardo7@gmail.com>
2025-04-19 10:30:24 -0500

    feat(client): crear entidad Client y probar persistencia

---
+++ client_service/src/main/java/com/e_commerce_distribuido/client_service/service/IClientService.java
@@ -0,0 +1,7 @@
+package com.e_commerce_distribuido.client_service.service;
+
+import com.e_commerce_distribuido.client_service.dto.ClientRequestDTO;
+
+public interface IClientService{
+    void createClient(ClientRequestDTO ClientRequestDTO);
+}
