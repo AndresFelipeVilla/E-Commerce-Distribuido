4caeb52d12d496ac4994b3cfc58708eb261a8d0f
santiago <ramossoloyo12345@gmail.com>
2025-05-06 16:49:37 -0500

    se actualiza la vista

--- producto_service/producto/app_producto/api/serializer.py
+++ producto_service/producto/app_producto/api/serializer.py
@@ -3,6 +3,7 @@ from app_producto.models import Producto
 from category.api.serializer import CategorySerializer
 
 class ProductoSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
     class Meta:
         model = Producto
         fields = ['id', 'name', 'description', 'price', 'category']
